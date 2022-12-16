3c69c22a44b863a3bd963f309b06d25332f7cefb
Bitaw <stickynotes5@yahoo.com>
2022-12-16 08:17:11 -0600

    Added more consts to server.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,11 +3,9 @@ const { User } = require("../../models");
 
 router.post("/", async (req, res) => {
   try {
-    const dbUserData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    const dbUserData = await User.create(
+      req.body
+    );
 
     req.session.save(() => {
       req.session.loggedIn = true;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+const path = require('path');
 const express = require("express");
 const session = require("express-session");
+const exphbs = require('express-handlebars');
 const routes = require("./controllers/index.js");
+const helpers = require('./utils/helpers');
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: "Tsuchinoko",
@@ -19,6 +24,9 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
