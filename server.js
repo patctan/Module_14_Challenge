877cf601f7c81a6b168f15cf0a2e88199c3f954d
Bitaw <stickynotes5@yahoo.com>
2023-01-24 22:18:40 -0600

    Added seed dependency.

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log("Now listening on: http://localhost:" + PORT));
 });
 
 // GIVEN a CMS-style blog site
